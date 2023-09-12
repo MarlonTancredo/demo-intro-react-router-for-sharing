6d9051157d4a22ac3ad84b773ac769206d4a8c44
unknown <marlon_tancredo@hotmail.com>
2023-09-12 14:07:46 +0100

    feat: implementing the page  mainLayout

--- src/components/layouts/main_layouts.tsx
+++ src/components/layouts/main_layouts.tsx
@@ -1,1 +1,16 @@
-export {}
+import Header from "../header/header"
+import { Outlet } from "react-router-dom"
+import Footer from "../footer/footer"
+
+const MainLayout = () => {
+    return (
+        <>
+            <Header />
+            <main className='p-6' style={{ flex: '1' }}>
+                <Outlet />
+            </main>
+            <Footer />
+        </>
+    )
+}
+export default MainLayout
