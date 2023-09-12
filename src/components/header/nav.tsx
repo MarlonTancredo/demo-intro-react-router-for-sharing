d3dff6158173c77915b67c6289ff11add21d3fd5
unknown <marlon_tancredo@hotmail.com>
2023-09-12 14:06:06 +0100

    feat: implementing navLink to reedirect the page

--- src/components/header/nav.tsx
+++ src/components/header/nav.tsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom"
 
-const Nav = () => <nav className='is-flex is-flex-grow-1 is-align-items-center' style={{width:'100%'}}>
-    <ul  style={{width:'100%'}}
-            className='is-flex is-flex-direction-row is-justify-content-space-evenly is-justify-content-space-evenly'>
-        <li>Home</li>
-        <li>Products</li>
-        <li>About</li>
-        <li>Help</li>
+const Nav = () => <nav className='is-flex is-flex-grow-1 is-align-items-center' style={{ width: '100%' }}>
+    <ul style={{ width: '100%' }}
+        className='is-flex is-flex-direction-row is-justify-content-space-evenly is-justify-content-space-evenly'>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/products"}>Products</Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/help"}>Help</Link>
     </ul>
 </nav>;
 
